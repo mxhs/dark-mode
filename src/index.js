4915b7883bdb1337df34ab176913e045c1d96cb3
mxhs <maxwellstofman@gmail.com>
2020-10-28 15:53:25 -0700

    First draft.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,30 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  
+  const handleDarkMode = (setDarkMode) => {
+    // const [darkMode, setDarkMode] = useState(false); 
+    // * Custom hooks can accept your useState initializations which are then able to be passed on to the child components
+    let localStorageDarkMode = window.localStorage.getItem('Dark Mode')
+    //v This means you're accessing page for the first time because null means there isn't a localStorage yet
+    if (localStorageDarkMode === null){
+      console.log('lsDM', localStorageDarkMode)
+      window.localStorage.setItem('Dark Mode', false)
+      localStorageDarkMode = false
+      console.log('after', localStorageDarkMode)
+    } 
+    if(setDarkMode !== null){
+      localStorageDarkMode = !localStorageDarkMode
+    }
+    return localStorageDarkMode
+  }
+
+  // This useEffect is going to run a function everytime we load the page. 
+  // This is what you use to check if there a mode saved in local storage, 
+  // and if not it will create one.
+  useEffect(() =>{
+    handleDarkMode()
+  },[])
 
   useEffect(() => {
     axios
@@ -20,8 +43,8 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <div className={handleDarkMode() ? "dark-mode App" : "App"}>
+      <Navbar darkMode={handleDarkMode} />
       <Charts coinData={coinData} />
     </div>
   );
